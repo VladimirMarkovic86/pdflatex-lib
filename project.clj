7a7ec1def2249a6ef7c88bcefaf6abc30926b090
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-09 17:29:29 +0200

    Task 66: Implemented charts in svg-lib and used it in sample-client.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.1"
+(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.2"
   :description "PDF LaTeX library"
   :url "http://github.com/VladimirMarkovic86/pdflatex-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
                  ]
 
   :min-lein-version "2.0.0"
