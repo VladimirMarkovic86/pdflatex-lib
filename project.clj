27c47792050b4649ec54eff3e6d9ce05f6b956f0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 15:50:45 +0200

    Task 88: Implemented droid-server project, it is as library to be used with AndroidServer android java project.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.4"
+(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.5"
   :description "PDF LaTeX library"
   :url "http://github.com/VladimirMarkovic86/pdflatex-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
                  ]
 
   :min-lein-version "2.0.0"
