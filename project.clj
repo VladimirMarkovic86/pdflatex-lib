baecde5653f431bf4053658f40b67e255d342b01
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:55:02 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.7"
+(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.8"
   :description "PDF LaTeX library"
   :url "http://github.com/VladimirMarkovic86/pdflatex-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
                  ]
 
   :min-lein-version "2.0.0"
@@ -12,5 +12,7 @@
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   
-  :jar-exclusions [#"^reports/"])
+  :jar-exclusions [#"^reports/"
+                   #"README.md$"
+                   #"LICENSE$"])
 
