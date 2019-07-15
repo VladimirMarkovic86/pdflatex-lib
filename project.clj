4a4814e9e671aceb79fde29a4f23e036a6a1d6bb
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:02:27 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.8"
+(defproject org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.9"
   :description "PDF LaTeX library"
   :url "http://github.com/VladimirMarkovic86/pdflatex-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
                  ]
 
   :min-lein-version "2.0.0"
@@ -12,7 +12,5 @@
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   
-  :jar-exclusions [#"^reports/"
-                   #"README.md$"
-                   #"LICENSE$"])
+  :jar-exclusions [#"^reports/"])
 
